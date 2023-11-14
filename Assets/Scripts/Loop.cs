196ae09da282ea135b8d7463e046c77c276124e8
connahKendrickMMU <44666644+connahKendrickMMU@users.noreply.github.com>
2023-11-14 18:36:21 +0000

    Added pause after loop
    The system will auto pause after the loop is done

--- Assets/Scripts/Loop.cs
+++ Assets/Scripts/Loop.cs
@@ -23,5 +23,7 @@ public class Loop : MonoBehaviour
         // a simple function to time a method
         Profiler.EndSample();
         Debug.Log("Update Time for " + loopTimes + " loops = " + (Time.realtimeSinceStartup - start));
+        // pause unity
+        Debug.Break();
     }
 }
